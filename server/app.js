67a1950a38468920e3960bc831a7c84bc1ac11a9
GINI TYSON <ginityson@yahoo.com>
2016-06-15 09:22:08 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var server = app.listen(8080, 'localhost', function() {
+  console.log("howdy universe from Gini's heroku solo on local server");
+});
+app.get('/', function (req, res) {
+  console.log("howdy from Gini's base url get heroku solo");
+  res.writeHead(200);
+  res.write("here I am in Gini's base url for heroku solo");
+  res.end();
+});
