fd7bc36e74dfe636ac15b2d111a6cfd6c2447584
GINI TYSON <ginityson@yahoo.com>
2016-06-15 20:25:21 -0500

    evening try

--- server/app.js
+++ server/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var server = app.listen(8080, 'localhost', function() {
+var server = app.listen(process.env.PORT || 3000);
+
+// var server = app.listen(8080, 'localhost', function() {
   console.log("howdy universe from Gini's heroku solo on local server");
-});
+//});
 app.get('/', function (req, res) {
   console.log("howdy from Gini's base url get heroku solo");
   res.writeHead(200);
